14970ba2021b6669ade86e5b0d8b952a29e257ed
WendelMattos23 <wmattos648@gmail.com>
2024-09-15 00:06:22 -0300

    Segundo Commit

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,29 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummary } from '../functions/get-week-summary'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server Running!')
+    console.log('HTTP server running')
   })
