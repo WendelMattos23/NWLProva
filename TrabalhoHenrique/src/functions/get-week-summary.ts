f167850338de527452cd35ca2c881e997cbdf677
WendelMattos23 <wmattos648@gmail.com>
2024-09-15 22:48:31 -0300

    quinto commit

--- TrabalhoHenrique/src/functions/get-week-summary.ts
+++ TrabalhoHenrique/src/functions/get-week-summary.ts
@@ -1,4 +1,4 @@
-import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+import { and, eq, gte, lte, sql } from 'drizzle-orm'
 import { db } from '../db'
 import { goalCompletions, goals } from '../db/schema'
 import dayjs from 'dayjs'
@@ -57,6 +57,15 @@ export async function getWeekSummary() {
       .groupBy(goalsCompletedInWeek.completedAtDate)
   )
 
+  type GoalsPerDay = Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+
   const result = await db
     .with(goalsCreatedUpToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
     .select({
@@ -68,7 +77,7 @@ export async function getWeekSummary() {
       (SELECT SUM(${goalsCreatedUpToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUpToWeek})
     `.mapWith(Number),
 
-      goalsPerDay: sql /*sql*/`
+      goalsPerDay: sql /*sql*/<GoalsPerDay>`
       JSON_OBJECT_AGG(
         ${goalsCompletedByWeekDay.completedAtDate},
         ${goalsCompletedByWeekDay.completions}
@@ -78,6 +87,6 @@ export async function getWeekSummary() {
     .from(goalsCompletedByWeekDay)
 
   return {
-    summary: result,
+    summary: result[0],
   }
 }
--- nlw/src/main.tsx
+++ nlw/src/main.tsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
-const rootElement = document.getElementById('root')
 
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  )
-} else {
-  console.error('Elemento com ID "root" não encontrado.')
-}
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+<QueryClientProvider client={queryClient}>
+<App />
+</QueryClientProvider>
+)
